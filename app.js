02e89a9aff6b802ea960a5216e3ed7f238cbac93
GenrihCrown <genrihcrown@gmail.com>
2021-12-18 18:25:39 +0300

    set-right-structure

--- app.js
+++
@@ -1,6 +0,0 @@
-const inputField = document.querySelector('.inputField input');
-const addTaskButton = document.querySelector('.addTaskButton');
-
-addTaskButton.addEventListener('click', function() {
-    console.log('click!')
-})
